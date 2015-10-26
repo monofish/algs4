6adaf78431184d42d74965d906b0cb105fd618db
monofish <SonicJamOnline@msn.com>
2015-10-26 05:44:40 -0400

    A client using class RandomizedQueue

---
+++ src/queues/Subset.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.*;
+
+public class Subset {
+
+	public static void main(String[] args) {
+
+		RandomizedQueue<String> queue = new RandomizedQueue<String>();  
+		int k = Integer.parseInt(args[0]); 
+		
+		while (!StdIn.isEmpty()) {  
+			String item = StdIn.readString();  
+			queue.enqueue(item);  
+		}  
+		while (k > 0){  
+			StdOut.println(queue.dequeue());  
+			k--;  
+		}  
+	}
+
+}
