9cce7942c136551925123586caa67039258abab2
monofish <SonicJamOnline@msn.com>
2015-10-26 20:52:57 -0400

    a more rigorous subset printing client
    It strictly uniformly outputs k items from the subset queue with length
    at most k

--- src/queues/Subset.java
+++ src/queues/Subset.java
@@ -1,20 +1,22 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Subset {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		RandomizedQueue<String> queue = new RandomizedQueue<String>();  
-		int k = Integer.parseInt(args[0]); 
-		
-		while (!StdIn.isEmpty()) {  
-			String item = StdIn.readString();  
-			queue.enqueue(item);  
-		}  
-		while (k > 0){  
-			StdOut.println(queue.dequeue());  
-			k--;  
-		}  
-	}
-
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();  
+        int k = Integer.parseInt(args[0]);
+        for (double count = 1; !StdIn.isEmpty(); count++) {
+            String item = StdIn.readString();
+            if (k < 0)
+                throw new IllegalArgumentException("Positive integer parameters required!");
+            else if (StdRandom.uniform() > 1 - k / count) { // If the item is chosen to add in to the queue;
+                if (count > k) queue.dequeue(); // If the queue already has k items in it (full), then dequeue one of them;
+                queue.enqueue(item);
+            } // Otherwise the item is NOT chosen, then do nothing
+        }
+        for (String str : queue) StdOut.println(str);
+   }
 }
