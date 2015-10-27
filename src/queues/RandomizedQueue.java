9d2e187880ad70fb4c6d193324fbb3a705206553
monofish <SonicJamOnline@msn.com>
2015-10-26 20:58:28 -0400

    Complete reconstruction
    Made the iteration process in a strict uniform fashion and mutually
    independent; simplied the whole data structure with less memory used;
    exceptions properly thrown.

--- src/queues/RandomizedQueue.java
+++ src/queues/RandomizedQueue.java
@@ -1,103 +1,112 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import edu.princeton.cs.algs4.*;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-	private int head, tail, length;
-	private Item[] randque;
+    private int last;
+    private Item[] randQ;
 
-	// construct an empty randomized queue
-	public RandomizedQueue() {
-		int capacity = 5;
-		head = 0;
-		tail = 0;
-		length = capacity;
-		randque = (Item[]) new Object[capacity];
-	}
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        int capacity = 2;
+        last = 0;
+        randQ = (Item[]) new Object[capacity];
+    }
 
-	// is the queue empty?
-	public boolean isEmpty() { return head == tail; }
+    // is the queue empty?
+    public boolean isEmpty() { return last == 0; }
 
-	// return the number of items on the queue
-	public int size() { return (tail - head) % length; }
+    // return the number of items on the queue
+    public int size() { return last; }
 
-	// add the item
-	public void enqueue(Item item) {
-		if (item == null) throw new java.lang.NullPointerException("Null item unacceptable");
-		
-		// If tail meets head, resize the queue first, then increment tail.
-		if (head == (tail + 1) % length) resize(2 * length);
-		randque[tail] = item;
-		tail = (tail + 1) % length;
-	}
-	
-	// remove and return a random item
-	public Item dequeue() {
+    // add the item
+    public void enqueue(Item item) {
+        if (item == null) throw new java.lang.NullPointerException("Null item unacceptable");
+        
+        // If last meets the end of the queue, then resize the queue by 2 times first
+        if (last == randQ.length) randQ = resize(2 * randQ.length);
+        randQ[last++] = item;
+    }
+    
+    // remove and return a random item
+    public Item dequeue() {
         if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-		int randIdx = randomIndex();
-		Item item = randque[randIdx];
-		randque[randIdx] = randque[head];
-		randque[head] = null; // Avoid loitering
-		head = (head + 1) % length;
-		int size = size();
-		if (size > 0 && size <= length / 4) resize(length / 2);
-		return item;
-	}
-	
-	// return (but do not remove) a random item
-	public Item sample() {
+        int randIdx = StdRandom.uniform(last); // generate a random index within the queue
+        Item item = randQ[randIdx];
+        randQ[randIdx] = randQ[--last];
+        randQ[last] = null; // assign it null to avoid loitering
+        
+        // If last meets the end of the queue, then resize the queue by 2 times first
+        if (last > 0 && last <= randQ.length / 4) randQ = resize(randQ.length / 2);
+        return item;
+    }
+    
+    // return (but do not remove) a random item
+    public Item sample() {
         if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-        return randque[randomIndex()];
-	}
-	
-	// return an independent iterator over items in random order
-	public Iterator<Item> iterator() {return new RandomizedArrayIterator();}         
+        return randQ[StdRandom.uniform(last)];
+    }
+    
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() { return new RandomizedArrayIterator(); }         
 
-	private class RandomizedArrayIterator implements Iterator<Item> {
-		private int i = 0;
+    private class RandomizedArrayIterator implements Iterator<Item> {
+        private int i;
+        private Item[] fullqueue;
 
-		public RandomizedArrayIterator() {
-			if (head > tail) {
-				StdRandom.shuffle(randque, 0, tail - 1);
-				StdRandom.shuffle(randque, head, length - 1);
-			}
-			else if (head < tail - 1) StdRandom.shuffle(randque, head, tail - 1); // There are at least 2 items in the queue.
-			else return;
-		}
-		
-		@Override
-		public boolean hasNext() { return i < size(); }
+        public RandomizedArrayIterator() {
+            i = 0;
+            fullqueue = resize(last); // generate a new but identical queue with full size
+            StdRandom.shuffle(fullqueue);
+        }
+        
+        @Override
+        public boolean hasNext() { return i < last; }
 
-		@Override
-		public Item next() {
-			if (i >= size()) throw new java.util.NoSuchElementException("Next item not exists");
-			else {
-				return randque[(head + (i++)) % length];
-			}
-		}
+        @Override
+        public Item next() {
+            if (!hasNext()) throw new java.util.NoSuchElementException("Next item not exists");
+            else return fullqueue[i++];
+        }
 
-		@Override
-		public void remove() { throw new java.lang.UnsupportedOperationException("Method remove() unsupported"); }
-	}
-	
-	private int randomIndex() { return (StdRandom.uniform(this.size()) + head) % length; }
-	
-	private void resize(int capacity) {
-		Item[] copy = (Item[]) new Object[capacity];
-		int i;
-		for(i = 0; i < size(); i ++)
-			copy[i] = randque[(head + i) % length];
-		randque = copy;
-		length = capacity;
-		head = 0;
-		tail = i;
-	}
-	
-	public static void main(String[] args) {
+        @Override
+        public void remove() { throw new java.lang.UnsupportedOperationException("Method remove() unsupported"); }
+    }
 
-		//Testing Client
-	
-	}
+    private Item[] resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < last; i++)
+            copy[i] = randQ[i];
+        return copy;
+    }
+    
+    public static void main(String[] args) {
 
+        //Testing Client
+        RandomizedQueue<Integer> queue = new RandomizedQueue<Integer>();
+
+        for (int i : queue)
+            StdOut.print(i+",");
+        StdOut.println(queue.size());
+        
+        for (int i = 100; i < 150; i++)
+            queue.enqueue(i);
+        StdOut.println(queue.size());
+
+        for (int i = 0; i < 20; i++)
+            StdOut.print(queue.dequeue()+",");
+        StdOut.println(queue.size());
+
+        for (int i = 900; i < 910; i++)
+            queue.enqueue(i);
+        StdOut.println(queue.size());
+
+        for (int i : queue)
+            StdOut.print(i+",");
+        StdOut.println(queue.size());
+        
+    }
 }
